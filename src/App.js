0e869c031dab9f5bf9ac2c19159e57dbab9df354
siddhu_from_sklm <codewithsiddhu@gmialo.com>
2024-08-22 14:50:42 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Tailwind CSS Example */}
+      <div className="text-center text-2xl text-blue-500">
+        Hello, Tailwind CSS!
+      </div>
+
+      {/* Bootstrap Example */}
+      <div className="text-center mt-4">
+        <button className="btn btn-primary">Hello, Bootstrap!</button>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/bootstrap/dist/css/bootstrap.min.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
